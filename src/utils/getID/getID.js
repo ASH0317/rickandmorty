02627cec4e443c99685acfee85ce2c804daec0a5
ASH0317 <prohordiriyov@yandex.ru>
2023-05-07 14:37:06 +0300

    ;)

--- src/utils/getID/getID.js
+++
@@ -1,8 +0,0 @@
-import { API_PARAM_PAGE } from "../../constants/api"
-
-export const getCharacterPageId = (url) => {
-    const pos = url.lastIndexOf(API_PARAM_PAGE);
-    const id = url.slice(pos+API_PARAM_PAGE.length, url.length);
-
-    return Number(id);
-}
